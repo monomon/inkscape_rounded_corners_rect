e7cecd7c25f73be5f167d377ba099596f8c2ad1e
Mois Moshev <mois@monomon.me>
2017-09-28 12:10:36 +0300

    * Create an error message for no rectangles selected
    * Localize the message
    * Break out the shape generation into separate method

--- rounded_corners.py
+++ rounded_corners.py
@@ -17,6 +17,9 @@ __license__ = "GPL"
 
 import simplepath
 import inkex
+import gettext
+
+inkex.localize()
 
 class RoundCorners(inkex.Effect):
     def __init__(self):
@@ -44,12 +47,56 @@ class RoundCorners(inkex.Effect):
                                      action="store", type="inkbool",
                                      default=True, help="Inherit style from rect")
 
+    @staticmethod
+    def createRoundedRect(parent, bbox, corner_radii):
+        x, y, w, h = bbox
+        tl, tr, br, bl = corner_radii
+
+        pathEl = inkex.etree.SubElement(parent, inkex.addNS("path", "svg"))
+        d = []
+
+        if tl > 0:
+            d.append(["M", [x + tl, y]])
+        else:
+            d.append(["M", [x, y]])
+
+        if tr > 0:
+            d.append(["L", [x + w - tr, y]])
+            d.append(["Q", [x + w, y, x + w, y + tr]])
+        else:
+            d.append(["L", [x + w, y]])
+
+        if br > 0:
+            d.append(["L", [x + w, y + h - br]])
+            d.append(["Q", [x + w, y + h, x + w - br, y + h]])
+        else:
+            d.append(["L", [x + w, y + h]])
+
+        if bl > 0:
+            d.append(["L", [x + bl, y + h]])
+            d.append(["Q", [x, y + h, x, y + h - bl]])
+        else:
+            d.append(["L", [x, y + h]])
+
+        if tl > 0:
+            d.append(["L", [x, y + tl]])
+            d.append(["Q", [x, y, x + tl, y]])
+        else:
+            d.append(["L", [x, y]])
+        d.append(["z", []])
+
+        pathEl.set("d", simplepath.formatPath(d))
+
+        return pathEl
+
     def effect(self):
         tl = float(self.options.tl)
         tr = float(self.options.tr)
         br = float(self.options.br)
         bl = float(self.options.bl)
-        
+
+        paths = []
+
         for id, node in self.selected.iteritems():
             if node.tag == inkex.addNS("rect", "svg"):
                 x = float(node.get("x", 0))
@@ -57,41 +104,11 @@ class RoundCorners(inkex.Effect):
                 w = float(node.get("width"))
                 h = float(node.get("height"))
 
-                pathEl = inkex.etree.SubElement(node.getparent(), inkex.addNS("path", "svg"))
-                d = []
-
-                if tl > 0:
-                    d.append(["M", [x + tl, y]])
-                else:
-                    d.append(["M", [x, y]])
-
-                if tr > 0:
-                    d.append(["L", [x + w - tr, y]])
-                    d.append(["Q", [x + w, y, x + w, y + tr]])
-                else:
-                    d.append(["L", [x + w, y]])
-
-                if br > 0:
-                    d.append(["L", [x + w, y + h - br]])
-                    d.append(["Q", [x + w, y + h, x + w - br, y + h]])
-                else:
-                    d.append(["L", [x + w, y + h]])
-
-                if bl > 0:
-                    d.append(["L", [x + bl, y + h]])
-                    d.append(["Q", [x, y + h, x, y + h - bl]])
-                else:
-                    d.append(["L", [x, y + h]])
-
-                if tl > 0:
-                    d.append(["L", [x, y + tl]])
-                    d.append(["Q", [x, y, x + tl, y]])
-                else:
-                    d.append(["L", [x, y]])
-                d.append(["z", []])
-
-                pathEl.set("d", simplepath.formatPath(d))
-
+                pathEl = RoundCorners.createRoundedRect(
+                        node.getparent(),
+                        (x, y, w, h),
+                        (tl, tr, br, bl)
+                    )
                 if self.options.inheritstyle:
                     # copy the styles
                     pathEl.set("style", node.get("style"))
@@ -99,9 +116,13 @@ class RoundCorners(inkex.Effect):
                     # set the same css class if there is one
                     if node.get("class"):
                         pathEl.set("class", node.get("class"))
+                paths.append(pathEl)
 
                 if self.options.remove:
                     node.getparent().remove(node)
+
+        if len(paths) == 0:
+            inkex.errormsg(_("Please select at least one rectangle."))
 
 if __name__ == "__main__":
     e = RoundCorners()
